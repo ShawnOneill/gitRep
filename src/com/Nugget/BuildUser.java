0e00051cd1a929f7b85dac314a2a7fbb1e76db51
Shawn O'Neill <shawn.oneill206@gmail.com>
2017-04-19 19:35:39 -0700

    commit BuildUser

--- src/com/Nugget/BuildUser.java
+++ src/com/Nugget/BuildUser.java
@@ -4,24 +4,11 @@ import java.util.Scanner;
 
 public class BuildUser{
 
-    public void main(String args){
-        String sentence = "";
-        String ltr= "";
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your sentence: ");
-        sentence = input.nextLine();
-        for (int i =0; i < sentence.length(); ++i){
-            ltr.charAt(i);
-           if(isVowel(ltr.charAt(i)) == true)
-                System.out.print(ltr);
-        }
+    public static void main(String[] args){
+
 
     }
-    public boolean isVowel(char letter){
-        if(letter == 'a' || letter == 'A' || letter == 'e' || letter == 'E' || letter == 'i' || letter == 'I'
-                || letter == 'o' || letter == 'O' || letter ==  'u' || letter == 'U' || letter == 'y'|| letter == 'Y')
-            return true;
-        else return false;
-    }
+
+
 
 }

62f74774fc652ec712e54ce66aa27cd341f8ca67
LAPTOP-PL6PKH0L\Shawn <ShawnNet>
2017-05-06 16:44:57 -0700

    added more posible responses

--- src/com/Nugget/BuildUser.java
+++ src/com/Nugget/BuildUser.java
@@ -9,7 +9,8 @@ public class BuildUser {
     public static void main(String[] args) {
         String userName = "";
         String thing = "";
-        String question = "";
+        String question= "";
+
         Scanner input = new Scanner(System.in);
         System.out.println("Who the hell are you?");
         userName = input.nextLine();
@@ -17,8 +18,7 @@ public class BuildUser {
             System.out.println("Fine I will call you Bob");
             userName = "Bob";
 
-            try {
-                Thread.sleep(4000L);
+            try {Thread.sleep(4000L);
             } catch (InterruptedException var7) {
                 var7.printStackTrace();
             }
@@ -29,14 +29,24 @@ public class BuildUser {
         if(thing.equals("")) {
             System.out.println("You look like a simple human to me.");
             thing = "human";
-
-            try {
-                Thread.sleep(4000L);
+            try {Thread.sleep(4000L);
             } catch (InterruptedException var6) {
                 var6.printStackTrace();
             }
         }
-
         System.out.println("Ok " + thing + " " + userName + " what do you want?");
+        question = input.nextLine();
+        while(question.equals("")){
+            System.out.println("What do you want?");
+            question = input.nextLine();
+        }
+        question = question.toLowerCase();
+        if (question.startsWith("i want a")){
+            System.out.println("Good luck with that!");
+        }
+        else if (question.startsWith("i want to play a game")){
+            System.out.println("You have come to the right place");
+        }
+        //TODO handle other questions
     }
 }
